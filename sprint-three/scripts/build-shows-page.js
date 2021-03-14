857102be8d08213452db9989cd0f7a40cd953e1f
felipevillajr <felipevilla2010@live.com>
2021-03-13 22:49:56 -0500

    added axios functionality (get and push) req to forms

--- sprint-three/scripts/build-shows-page.js
+++ sprint-three/scripts/build-shows-page.js
@@ -2,61 +2,125 @@
 
 
 
-const showsArray = [
-    {  
-        "DATE":'Mon Dec 17 2018',
-        "VENUE":'Ronald Lane',
-        "LOCATION": "San Fancisco, CA"
-    },
-    {  
-        "DATE":'Tue Jul 18 2019',
-        "VENUE":'Pier 3 East',
-        "LOCATION": "San Fancisco, CA"
-    },
-    { 
-        "DATE":'Fri Jul 22 2019',
-        "VENUE":'View Loungue',
-        "LOCATION": "San Fancisco, CA"
-    },
-    { 
-        "DATE":'Sat Aug 12 2019',
-        "VENUE":'Hyatt Agency',
-        "LOCATION": "San Fancisco, CA"
-    },
-    { 
-        "DATE":'Fri Sep 05 2019',
-        "VENUE":'Moscow Center',
-        "LOCATION": "San Fancisco, CA"
-    },
-    { 
-        "DATE":'Wed Aug 11 2019',
-        "VENUE":'Press Club',
-        "LOCATION": "San Fancisco, CA"
-    },
-];
 
 
-function displayShows(showsArray) {
-    let showsDivHTML= "";
 
-    showsArray.forEach((oneShow) => {
+const api_key = '?api_key=2e2b5066-9b85-43e3-b2d6-e932f9a41a5d'
+
+const urlComments = `https://project-1-api.herokuapp.com/showdates${api_key}`;
+
+
+const form = document.getElementById('commentForm');
+
+const comment = document.querySelector('#comment__submmited')
+
+axios.get(urlComments)
+    .then((res) => {
+    const comments = res.data
+
+    let commentDivHTML= document.querySelector("#comment__submmited");
+
+
+    let myNewHTML = comments.map(
+        child => {
+            return (
+        `<div id="comment__section">
+            <div id="userimg" ></div>
+            <div id= "timestamp">${new Date(child.timestamp).toLocaleString().split(',')[0]}</div>
+            <div id= "username">${child.name}</div>
+            <p id= "user__comment">${child.comment}</p>
+        </div>`
+            )
+        }
+        )
+        .join('')
+        commentDivHTML.innerHTML += myNewHTML ;
         
-        let showsBeDiv = `
-            <div id="shows__section">
-                <lable id= "shows__date">DATE</lable>
-                <div id= "date">${oneShow.DATE}</div>
-                <lable id= "shows__venue">VENUE</lable>
-                <div id= "venue">${oneShow.VENUE}</div>
-                <lable id= "shows__location">LOCATION</lable>
-                <p id= "location">${oneShow.LOCATION}</p>
-                <div>
-                <input type="button" value="BUY TICKETS">
-                </div>
-            </div>`;
-        showsDivHTML = showsDivHTML + showsBeDiv;
-    });
-    const divEl = document.getElementById('shows__list');
-    divEl.innerHTML = showsDivHTML;
-};
+});
 
-displayShows(showsArray)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const showsArray = [
+//     {  
+//         "DATE":'Mon Dec 17 2018',
+//         "VENUE":'Ronald Lane',
+//         "LOCATION": "San Fancisco, CA"
+//     },
+//     {  
+//         "DATE":'Tue Jul 18 2019',
+//         "VENUE":'Pier 3 East',
+//         "LOCATION": "San Fancisco, CA"
+//     },
+//     { 
+//         "DATE":'Fri Jul 22 2019',
+//         "VENUE":'View Loungue',
+//         "LOCATION": "San Fancisco, CA"
+//     },
+//     { 
+//         "DATE":'Sat Aug 12 2019',
+//         "VENUE":'Hyatt Agency',
+//         "LOCATION": "San Fancisco, CA"
+//     },
+//     { 
+//         "DATE":'Fri Sep 05 2019',
+//         "VENUE":'Moscow Center',
+//         "LOCATION": "San Fancisco, CA"
+//     },
+//     { 
+//         "DATE":'Wed Aug 11 2019',
+//         "VENUE":'Press Club',
+//         "LOCATION": "San Fancisco, CA"
+//     },
+// ];
+
+
+// function displayShows(showsArray) {
+//     let showsDivHTML= "";
+
+//     showsArray.forEach((oneShow) => {
+        
+//         let showsBeDiv = `
+//             <div id="shows__section">
+//                 <lable id= "shows__date">DATE</lable>
+//                 <div id= "date">${oneShow.DATE}</div>
+//                 <lable id= "shows__venue">VENUE</lable>
+//                 <div id= "venue">${oneShow.VENUE}</div>
+//                 <lable id= "shows__location">LOCATION</lable>
+//                 <p id= "location">${oneShow.LOCATION}</p>
+//                 <div>
+//                 <input type="button" value="BUY TICKETS">
+//                 </div>
+//             </div>`;
+//         showsDivHTML = showsDivHTML + showsBeDiv;
+//     });
+//     const divEl = document.getElementById('shows__list');
+//     divEl.innerHTML = showsDivHTML;
+// };
+
+// displayShows(showsArray)
+
+
+
+
--- sprint-three/scripts/index-page.js
+++ sprint-three/scripts/index-page.js
@@ -1,99 +1,61 @@
 
 
+const api_key = '?api_key=2e2b5066-9b85-43e3-b2d6-e932f9a41a5d'
 
-
-// comment form selector 
-const commentForm = document.querySelector(".comment--form");
-
-
-
-const commentArray = [
-    {  
-        "name":'Micheal Lyons',
-        "comment":'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed',
-        "timestamp": "12/18/2018"
-    },
-    {  
-        "name":'Gary Wong',
-        "comment": "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself!",
-        "timestamp": "12/12/2018"
-    },
-    { 
-        "name":'Theodore Duncan',
-        "comment":"How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He's definitely my favorite ever!" ,
-        "timestamp": "11/15/2018"
-    },
-];
-
-function displayCommentsDivs(commentArray) {
-    let commentDivHTML= "";
-
-    commentArray.forEach((oneComment) => {
-        
-        let  commentBeDiv = `
-            <div id="comment__section">
-                <div id="userimg""></div>
-                <div id= "timestamp">${oneComment.timestamp}</div>
-                <div id= "username">${oneComment.name}</div>
-                <p id= "user__comment">${oneComment.comment}</p>
-                
-            </div>`;
-        commentDivHTML = commentDivHTML + commentBeDiv;
-    });
-    const divEl = document.getElementById('comment__submission');
-    divEl.innerHTML = commentDivHTML;
-};
-
-displayCommentsDivs(commentArray)
-
-
-// const addComment = (function(){
-    
-//     return(addObj);
-
-// });
+const urlComments = `https://project-1-api.herokuapp.com/comments${api_key}`;
 
 
 
-commentForm.addEventListener("submit", (e) => {
-    //default setting remover
-    e.preventDefault();
-    
-    // targeting  comment values
-    // const name = e.target.name.value;
-    // const comment = e.target.textarea.value;
-    // const timestamp = new Date();
-    
-    const name = document.getElementById('name__form').value;
-    const comment = document.getElementById('textarea').value;
-    const timestamp =  new Date().toLocaleString().split(',')[0];
 
+const form = document.getElementById('commentForm');
 
+const comment = document.querySelector('#comment__submmited')
 
-    // new array
-    const commentArray = [];
-    
-    commentArray.push({name,comment,timestamp});
-    
+axios.get(urlComments)
+    .then((res) => {
+    const comments = res.data
+
     let commentDivHTML= document.querySelector("#comment__submmited");
-    
-    let  commentBeDiv = `
-            <div id="comment__section">
-                <div id="userimg" ></div>
-                <div id= "timestamp">${commentArray[0].timestamp}</div>
-                <div id= "username">${commentArray[0].name}</div>
-                <p id= "user__comment">${commentArray[0].comment}</p>
-            </div>`;
-        commentDivHTML.innerHTML += commentBeDiv;
-        
 
-    console.log(commentArray)
-    return;
+
+    let myNewHTML = comments.map(
+        child => {
+            return (
+        `<div id="comment__section">
+            <div id="userimg" ></div>
+            <div id= "timestamp">${new Date(child.timestamp).toLocaleString().split(',')[0]}</div>
+            <div id= "username">${child.name}</div>
+            <p id= "user__comment">${child.comment}</p>
+        </div>`
+            )
+        }
+        )
+        .join('')
+        commentDivHTML.innerHTML += myNewHTML ;
+        
 });
 
 
 
 
 
-
-
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    axios
+        .post(urlComments, {
+            name: e.target.name.value,
+            comment: e.target.textarea.value,
+        })
+        .then((res) => {
+            const oldDiv = document.querySelector('#comment__submmited');
+            const myNewHTML = document.createElement('div');
+            myNewHTML.setAttribute("id", "comment__section");
+            myNewHTML.innerHTML = `
+            <div id="userimg" ></div>
+            <div id= "timestamp">${new Date(res.data.timestamp).toLocaleDateString('en-US')}</div>
+            <div id= "username">${res.data.name}</div>
+            <p id= "user__comment">${res.data.comment}</p>
+            `;
+            oldDiv.prepend(myNewHTML);
+        })
+})
